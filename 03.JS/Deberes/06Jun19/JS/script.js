c1e7f8eee4ef6b4e1fb4c1a97258f6b7e3310771
hfzespinoz4 <harvin.zuniga@gmail.com>
2019-06-06 13:54:25 +0200

    Deberes de JS para el 06Junio

--- 03.JS/Deberes/06Jun19/JS/script.js
+++ 03.JS/Deberes/06Jun19/JS/script.js
@@ -1,1 +1,11 @@
 //Archivo con código JS
+
+var nombre;
+var edad;
+
+nombre = prompt("Ingresa tu nombre:");
+edad = prompt("Ingresa tu edad:");
+document.write(nombre + "  tiene  " + edad + " años de edad.");
+console.log ( nombre + " tiene " + edad + "de edad.");
+alert(nombre + " tiene " + edad + " años de edad.");
+
