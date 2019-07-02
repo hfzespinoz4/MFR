6280c20af8a0047a3cbf5e253b06ba9c73d71b59
hfzespinoz4 <harvin.zuniga@gmail.com>
2019-07-02 14:30:20 +0200

    Juego Xavi JS Revisado

--- 03.JS/Clases/27Jun19/Juego/script.js
+++ 03.JS/Clases/27Jun19/Juego/script.js
@@ -1,34 +1,35 @@
-const getRandomInt = (max) => {                        // Fx que devuelve un valor aleatorio
-    return Math.floor(Math.random(max));
-}
-getRandomInt();
-
-const randomNumber = getRandomInt(100);                      // Número Generado por la PC
-let userNumber = document.getElementById("number").value;    //Numero que ingresa el usuario
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+      }
+getRandomInt(100);
+const randomNumber = getRandomInt(100);                  // Número Generado por la PC
+const userNumber = document.querySelector("#number");    //Numero que ingresa el usuario
 const printTryout = document.getElementById("intentos");
 const printLifes = document.getElementById("restantes");
 let tryout = 0;     // El numero de intentos
 let lifes = 10;     // Cantidad intentos restantes.
 
 const comprobacion = () =>{
-    if (lifes.value<=10) {
-        if (userNumber.value < randomNumber.value){
-            alert("Te has pasado");    
-        }
-        else if (userNumber.value > randomNumber.value){
+    if (userNumber.value <= 100){
+        if (userNumber.value < randomNumber){
+            alert("Te has pasado");
+            console.log(userNumber + " es menor que " + randomNumber);    
+            }
+        else if (userNumber.value > randomNumber){
             alert("Te has ido por arriba");    
-        }
-        else if (userNumber.value === randomNumber.value){
+            console.log(userNumber + " es mayor que " + randomNumber)
+            }
+        else if (userNumber.value === randomNumber){
             alert("¡Felicidades, has acertado el número!");
-        }
+            console.log(userNumber + " es igual que " + randomNumber)
+            }
+    }
     else {
-       alert("Asegurate de haber ingresado un número");  
-         }      
-    }  
-}
+        alert("Asegurate de haber ingresado un número en el rango indicado");  
+         }        
 lifes=lifes-1;
 tryout=tryout+1;
-
+}
 document.getElementById('intentos').innerHTML=tryout;
-document.getElementById('restantes').innerHTML= lifes;
+document.getElementById('restantes').innerHTML=lifes;
 document.getElementById('buttom').addEventListener('click', comprobacion);
