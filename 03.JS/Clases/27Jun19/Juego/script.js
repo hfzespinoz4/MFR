355bdb9a00a529a73951150f5dbe4509cdeeec9e
hfzespinoz4 <harvin.zuniga@gmail.com>
2019-07-04 12:59:21 +0200

    Ejercicio Revisado

--- 03.JS/Clases/27Jun19/Juego/script.js
+++ 03.JS/Clases/27Jun19/Juego/script.js
@@ -1,6 +1,12 @@
 const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
       }
+
+const restartGame = () =>{
+    location.reload();
+    userNumber.value="";
+    userNumber.focus();
+}
 const randomNumber = getRandomInt(100);                  // Número Generado por la PC
 const userNumber = document.querySelector("#number");    // Numero que ingresa el usuario
 const printTryout = document.getElementById("intentos"); 
@@ -10,7 +16,7 @@ let tryout = 0;     // El numero de intentos
 let lifes = 10;     // Cantidad intentos restantes.
 
 sendNumber.addEventListener('click', function(event){
-    if (lifes > 0  && userNumber.value<100){
+    if (lifes > 0  && userNumber.value<=100){
         if (userNumber.value < randomNumber){
             alert("Te has pasado ");
             console.log("Te has pasado " + userNumber.value + " es menor que " + randomNumber);    
@@ -19,18 +25,30 @@ sendNumber.addEventListener('click', function(event){
             alert("Te has ido por arriba ");
             console.log("Te has ido por arriba " + userNumber.value + " es mayor que " + randomNumber);
             }
-        else if (userNumber.value === randomNumber){
+        else if (userNumber.value == randomNumber){
             alert("¡Felicidades, has acertado el número!");
-        }
+            console.log("¡Felicidades, has acertado el número!");
+            restartGame();
+            }
         else {
-            alert("Asegurate de haber ingresado un número en el rango indicado");  
-             }
+            alert("Asegurate de haber ingresado un número"); 
+            console.log("Asegurate de haber ingresado un número"); 
+            }
     }
+    else if (userNumber.value > 100){
+        alert("Asegurate de haber ingresado un número en el rango indicado");  
+        console.log(userNumber.value + " excede el rango de números permitidos");
+    } 
+    else if (isNaN(userNumber.value)){
+        alert("Debes ingresar un número");
+        console.log("Debes ingresar un número");
+        return true;
+        }
     else {
         alert("No tienes mas intentos"); 
-        return; 
+        restartGame();
          }
-    userNumber.value= " ";
+    userNumber.value="";
     userNumber.focus();
     lifes--;
     tryout++;
