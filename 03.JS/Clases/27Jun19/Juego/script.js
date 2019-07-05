b55a18eab3ec5b3e0b6816a311746537a5e8c41c
hfzespinoz4 <harvin.zuniga@gmail.com>
2019-07-05 13:03:55 +0200

    El Juego de Xavi revisado en clases 04/Jul

--- 03.JS/Clases/27Jun19/Juego/script.js
+++ 03.JS/Clases/27Jun19/Juego/script.js
@@ -1,60 +1,54 @@
 const getRandomInt = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
-      }
+    return Math.floor(Math.random() * max);
+}
 
-const restartGame = () =>{
+const restartGame = () => {
     location.reload();
-    userNumber.value="";
+    userNumber.value = "";
     userNumber.focus();
 }
+
 const randomNumber = getRandomInt(100);                  // Número Generado por la PC
 const userNumber = document.querySelector("#number");    // Numero que ingresa el usuario
-const printTryout = document.getElementById("intentos"); 
-const printLifes = document.getElementById("restantes");
-const sendNumber = document.querySelector('#buttom');
-let tryout = 0;     // El numero de intentos
+const tryoutsElement = document.getElementById("tryouts"); 
+const lifesElement = document.getElementById("lifes");
+const sendNumber = document.getElementById('random-num-val');
+let tryouts = 0;     // El numero de intentos
 let lifes = 10;     // Cantidad intentos restantes.
 
-sendNumber.addEventListener('click', function(event){
-    if (lifes > 0  && userNumber.value<=100){
-        if (userNumber.value < randomNumber){
-            alert("Te has pasado ");
-            console.log("Te has pasado " + userNumber.value + " es menor que " + randomNumber);    
+const showMessages = (text, comparison) => {
+    alert(text);
+    console.log(text + userNumber.value + comparison + randomNumber);
+
+}
+
+sendNumber.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const value = Number(userNumber.value);
+    if (lifes > 0 ) {
+        if (value < randomNumber) {
+            showMessages("Te has pasado ", " es menor ");    
             }
-        else if (userNumber.value > randomNumber){
-            alert("Te has ido por arriba ");
-            console.log("Te has ido por arriba " + userNumber.value + " es mayor que " + randomNumber);
+        else if (value > randomNumber){
+            showMessages("Te has ido por arriba ", " es mayor ");
             }
-        else if (userNumber.value == randomNumber){
-            alert("¡Felicidades, has acertado el número!");
-            console.log("¡Felicidades, has acertado el número!");
+        else if (value === randomNumber) {
+            // alert("¡Felicidades, has acertado el número!");
+            // console.log("¡Felicidades, has acertado el número!");
+            showMessages("¡Felicidades, has acertado el número!");
             restartGame();
             }
-        else {
-            alert("Asegurate de haber ingresado un número"); 
-            console.log("Asegurate de haber ingresado un número"); 
-            }
     }
-    else if (userNumber.value > 100){
-        alert("Asegurate de haber ingresado un número en el rango indicado");  
-        console.log(userNumber.value + " excede el rango de números permitidos");
-    } 
-    else if (isNaN(userNumber.value)){
-        alert("Debes ingresar un número");
-        console.log("Debes ingresar un número");
-        return true;
-        }
     else {
         alert("No tienes mas intentos"); 
         restartGame();
-         }
+        }
     userNumber.value="";
     userNumber.focus();
     lifes--;
-    tryout++;
-    printTryout.innerHTML=tryout;
-    printLifes.innerHTML=lifes;
-    return false;   
+    tryouts++;
+    tryoutsElement.innerHTML=tryouts;
+    lifesElement.innerHTML=lifes;
 })
-printTryout.innerHTML=tryout;
-printLifes.innerHTML=lifes;
+tryoutsElement.innerHTML=tryouts;
+lifesElement.innerHTML=lifes;
