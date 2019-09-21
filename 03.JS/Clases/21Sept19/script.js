65832cbd870e3207ffee45a1504f57a01f7ac46a
hfzespinoz4 <harvin.zuniga@gmail.com>
2019-09-21 10:26:38 +0200

    Actualizacion del repositorio

---
+++ 03.JS/Clases/21Sept19/script.js
@@ -0,0 +1,8 @@
+new Vue ({
+    el: '#app',
+    data: {
+        message: 'Hola Mundo!',
+        tooltipMessage: `Hovered element at ${new Date()}`,
+        showFirstMessage: true
+    }
+});
--- 03.JS/Deberes/Firebase/firebase.js
+++ 03.JS/Deberes/Firebase/firebase.js
@@ -1,1 +1,58 @@
-https://mfrbase.firebaseio.com/
+
+
+function apicall (){
+    const url = "https://mfrbase.firebaseio.com";
+    const firebaseConfig = {
+        apiKey: "AIzaSyDfYhehG0n5I7f-BQjU4hcOP8P4pOaLsd4",
+        authDomain: "mfrbase.firebaseapp.com",
+        databaseURL: "https://mfrbase.firebaseio.com",
+        projectId: "mfrbase",
+        storageBucket: "mfrbase.appspot.com",
+        messagingSenderId: "930273683907",
+        appId: "1:930273683907:web:9460c3574b1633a8c93576"
+    };
+      // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    fetch(url, firebaseConfig)
+    .then (response => {
+        return response.json();
+    })
+    .then(function(myJson){
+        console.log(myJson);
+    });
+}
+
+window.addEventListener('load', apicall());
+
+
+
+
+
+
+
+/*
+fetch('http://example.com/movies.json')
+.then(function(response) {
+  return response.json();
+})
+.then(function(myJson) {
+  console.log(myJson);
+});
+
+$.getscript("js/Notas_JS.js", function(){
+           agregarRefacciones();
+        });
+
+
+
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/6.6.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  
+</script> */
